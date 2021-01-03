38a0d1ec4a0a242a2a596650b222205083c97579
Hazrat Ali <iloveyii@yahoo.com>
2021-01-03 23:08:10 +0100

    Bind .fxml and controller

---
+++ src/sample/Controller.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.scene.control.Button;
+
+public class Controller {
+    public Button btnRegister;
+
+    public Controller() {
+        System.out.println("hi");
+    }
+
+    public void btnRegisterClick() {
+        System.out.println("Btn clicked");
+    }
+}
