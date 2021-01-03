f4c9b6b60a2f3e5737cc95ccfbd2c33122184e00
Hazrat Ali <iloveyii@yahoo.com>
2021-01-04 00:00:37 +0100

    Passing parameters to Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,12 +4,17 @@ import javafx.scene.control.Button;
 
 public class Controller {
     public Button btnRegister;
-
+    public static int a = 10;
     public Controller() {
         System.out.println("hi");
     }
 
     public void btnRegisterClick() {
-        System.out.println("Btn clicked");
+        System.out.println("Btn clicked" + a);
+    }
+
+    public static void setList(int b) {
+        a = b;
+        System.out.println("Controller " + a);
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+
+        Controller controller = loader.getController();
+        controller.setList(103);
+
         primaryStage.setTitle("Register emails");
-        primaryStage.setScene(new Scene(root, 500, 300));
+        primaryStage.setScene(new Scene(loader.load(), 500, 300));
         primaryStage.show();
     }
 
